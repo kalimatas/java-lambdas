f3ebf5d6670a82889af6b70c1a120a573b530eca
Alexander Guz <kalimatas@gmail.com>
2018-05-23 17:28:56 +0200

    Some examples of Stream usages

--- src/chapter3/Chapter3.java
+++ src/chapter3/Chapter3.java
@@ -4,6 +4,11 @@ import domain.Artist;
 import domain.SampleData;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.Character.isDigit;
 
 public class Chapter3 {
     public static void main(String[] args) {
@@ -14,9 +19,20 @@ public class Chapter3 {
         var threeArtists = SampleData.getThreeArtists();
 
         long count = threeArtists.stream()
-                            .filter(artist -> artist.isFrom("UK"))
-                            .count();
+                                 .filter(artist -> artist.isFrom("UK"))
+                                 .count();
 
         System.out.println(count);
+
+        List<String> letters = Stream.of("a", "b", "c")
+                                     .collect(Collectors.toList());
+
+        List<String> upper = Stream.of("a", "b", "c")
+                                   .map(String::toUpperCase)
+                                   .collect(Collectors.toList());
+
+        List<String> withDigit = Stream.of("a", "2ef", "3bbb")
+                                       .filter(s -> isDigit(s.charAt(0)))
+                                       .collect(Collectors.toList());
     }
 }
